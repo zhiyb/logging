2da92b2d8e3770669ac62362139309b8e3f87aec
Norman Zhi <normanzyb@gmail.com>
2023-10-07 14:20:40 +0000

    www: Change graph_windows and graph_sensors to use plotly too

--- www/functions.js
+++ www/functions.js
@@ -502,7 +502,71 @@ function get_sensor_unit(type) {
 
 
 
+let traces = [];
+let tracecfgs = {};
+
+function draw_chart(domkey, cfg) {
+  let ajaxs = [];
+  for (let i = 0; i < cfg.length; i++) {
+    let c = cfg[i];
+    tracecfgs[c.id] = {
+      legendrank: i,
+      legendgroup: c.id,
+      legendgrouptitle: {text: `- ${c.id} -`},
+      yaxis: `y${i == cfg.length-1 ? "" : cfg.length-i}`,
+      type: {line: "scattergl", stack: "scatter"}[c.type],
+      mode: c.type == "line" ? "lines" : undefined,
+      stackgroup: c.type == "stack" ? c.id : undefined,
+      line: {"width": 1},
+    };
+    ajaxs.push($.getJSON(c.url, c.func));
+  }
+
+  $.when(...ajaxs).done(function () {
+    let layout = {
+      autosize: true,
+      height: 600 + 300 * cfg.length,
+      margin: {
+        t: 20,
+        b: 80,
+        l: 60,
+        r: 20,
+      },
+      showlegend: true,
+      legend: {
+        orientation: "h",
+        //yanchor: "bottom",
+        //y: 1,
+        groupclick: "toggleitem",
+      },
+    };
+
+    // yaxis domain(location) and title
+    let dsize = 1 / cfg.length;
+    for (let i = 1; i <= cfg.length; i++) {
+      layout["yaxis" + (i == 0 ? "" : i)] = {
+        title: cfg[cfg.length-i].ytitle,
+        domain: [dsize * (i-1), dsize * i],
+      };
+    }
+
+    Plotly.react(domkey, traces, layout, {responsive: true});
+  });
+}
+
+function add_traces(id, values) {
+  for (let key in values) {
+    let v = values[key];
+    for (let c in tracecfgs[id])
+      v[c] = tracecfgs[id][c];
+    v.name = key;
+    traces.push(v);
+  }
+}
+
+
+
 export {
-  add_chart, parse_ts, get_sensor_unit,
+  add_chart, draw_chart, add_traces, parse_ts, get_sensor_unit,
   schemeTableau10, schemeTableau10Light, schemeTableau10Medium, schemeTableau20,
 };
